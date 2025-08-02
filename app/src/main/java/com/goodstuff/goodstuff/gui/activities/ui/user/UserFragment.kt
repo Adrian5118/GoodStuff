1c90209a0431d1f5c6e303002ddfa3fb2a351107
farhanperm <farhanperm@gmail.com>
2025-08-02 15:03:10 +0700

    update 2

--- app/src/main/java/com/goodstuff/goodstuff/gui/activities/ui/user/UserFragment.kt
+++ app/src/main/java/com/goodstuff/goodstuff/gui/activities/ui/user/UserFragment.kt
@@ -1,5 +1,6 @@
 package com.goodstuff.goodstuff.gui.activities.ui.user
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,14 +8,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.goodstuff.goodstuff.LoginActivity
 import com.goodstuff.goodstuff.databinding.FragmentUserBinding
 
 class UserFragment : Fragment() {
 
     private var _binding: FragmentUserBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,16 +22,25 @@ class UserFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val userViewModel =
-            ViewModelProvider(this).get(UserViewModel::class.java)
+        val userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
 
         _binding = FragmentUserBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // Menampilkan text dari ViewModel
         val textView: TextView = binding.textNotifications
         userViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        // Aksi logout
+        binding.logoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
         return root
     }
 
